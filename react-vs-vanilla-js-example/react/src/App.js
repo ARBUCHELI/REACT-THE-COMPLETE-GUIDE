d71e8c35e3edeb45bbbf397ef4050b960dd58593
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-05-29 21:21:19 +0300

    Add files via upload

---
+++ react-vs-vanilla-js-example/react/src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Learn React Again' />
+      <Todo text='Learn React over and over Again' />
+    </div>
+  );
+}
+
+export default App;
